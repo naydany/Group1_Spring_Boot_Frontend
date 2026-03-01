4c846642188bbec15f8fbf00fd396bd68f626381
Dany Nay <dany.nay@axivit.com>
2026-03-01 20:53:01 +0700

    add some code

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/Pages/publish/HomeView.vue'
-import PublishLayout from '@/components/layouts/PublishLayout.vue'
-import RegisterView from '@/views/Pages/publish/auth/RegisterView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/Pages/publish/HomeView.vue";
+import PublishLayout from "@/components/layouts/PublishLayout.vue";
+import RegisterView from "@/views/Pages/publish/auth/RegisterView.vue";
+import AuthLogin from "@/views/Pages/publish/auth/AuthLogin.vue";
 const routes = [
-   {
-      path: '/register',
-      name:'Register',
-      component: RegisterView
-
-    },
-  { 
-   
-    path: '/', 
-    component: PublishLayout,
-    children:[{
-      path: '',
-      name:'Home',
-      component: HomeView
-
-    }
-    
-
-    ]
+  {
+    path: "/login",
+    name: "Login",
+    component: AuthLogin,
   },
-  
-]
+  {
+    path: "/register",
+    name: "Register",
+    component: RegisterView,
+  },
+  {
+    path: "/",
+    component: PublishLayout,
+    children: [
+      {
+        path: "",
+        name: "Home",
+        component: HomeView,
+      },
+    ],
+  },
+];
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
-export default router
+});
+export default router;
