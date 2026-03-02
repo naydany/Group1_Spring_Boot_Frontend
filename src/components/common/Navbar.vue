6296b5a4792005fe886792c93f40f4a52d8ffbc9
Dany Nay <dany.nay@axivit.com>
2026-03-02 18:25:12 +0700

    update-code

--- src/components/common/Navbar.vue
+++ src/components/common/Navbar.vue
@@ -25,7 +25,7 @@
         <ul class="navbar-nav ms-auto fs-4 ">
 
           <li class="nav-item">
-            <router-link class="nav-link" to="/">Home</router-link>
+            <router-link class="nav-link" to="/home">Home</router-link>
           </li>
 
           <li class="nav-item">
